bab5ff867cbc09e32eb4b3ab0a825dbaf7183f9e
DESKTOP-E944KI0\bruno <bruno_diego5@hotmail.com>
2018-10-06 01:16:20 -0300

    novo build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,16 @@
-export const environment = {
+/* export const environment = {
   production: true,
   apiUrl: 'https://editalsniffer-api.herokuapp.com',
 
   tokenWhitelistedDomains: [ new RegExp('editalsniffer-api.herokuapp.com') ],
+  tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
+}; */
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+
+  tokenWhitelistedDomains: [ new RegExp('localhost:8080') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
 
