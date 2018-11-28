dfc10c83cb22c52a1ccc0431db46ecb4e9f24a22
DESKTOP-E944KI0\bruno <bruno_diego5@hotmail.com>
2018-11-27 23:25:51 -0200

    teste

--- src/app/itens/itens-cadastro/itens-cadastro.component.ts
+++ src/app/itens/itens-cadastro/itens-cadastro.component.ts
@@ -2,11 +2,13 @@ import { Title } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
 
 import { MessageService } from 'primeng/components/common/messageservice';
 
 import { ErrorHandlerService } from './../../core/error-handler.service';
-import { ItemService } from './../item.service';
+import { ItemFiltro, ItemService } from './../item.service';
 import { Item } from './../../core/model/Item';
 
 @Component({
@@ -18,6 +20,14 @@ export class ItensCadastroComponent implements OnInit {
 
   formulario: FormGroup;
   ids: number[] = [];
+
+  itensEncontrado: Observable<any[]>;
+  private searchTerms = new Subject<string>();
+
+  ultimoKeyPress: number;
+  filtro = new ItemFiltro();
+  /* startAt = new Subject();
+  endAt = new Subject(); */
 
   constructor(
     private itemService: ItemService,
@@ -38,6 +48,15 @@ export class ItensCadastroComponent implements OnInit {
     if (idItem) {
       this.carregarItem(idItem);
     }
+
+    this.itensEncontrado = this.searchTerms
+      .pipe(debounceTime(300),
+        distinctUntilChanged(),
+        switchMap(term => term ? this.itemService.pesquisar(term) : Observable.of<any[]>([]))),
+        catchError(error => {
+        console.log(error);
+        return Observable.of<any[]>([]);
+      });
   }
 
   configurarFormulario() {
@@ -122,5 +141,28 @@ export class ItensCadastroComponent implements OnInit {
       if (excluirControl) {
         this.excluir(itemIndex);
       }
+  }
+
+  pesquisarItens(event) {
+    if (event.timeStamp - this.ultimoKeyPress > 200) {
+      const  q = event.target.value;
+      /* this.startAt.next(q);
+      this.endAt.next(q + '\uf8ff'); */
+
+      this.filtro.pagina = 0;
+      this.filtro.descricao = q;
+
+      console.log(this.filtro.descricao);
+
+      this.itemService.pesquisar(this.filtro)
+      .subscribe(dados => {
+        /* this.totalRegistros = dados.totalElements; */
+        this.itensEncontrado = dados.content;
+      },
+      erro => this.errorHandler.handle(erro));
+    }
+
+    this.ultimoKeyPress = event.timeStamp;
+
   }
 }
